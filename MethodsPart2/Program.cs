bb05e5d12e6afc04680dd872019771240ff58984
Ben Cofield <benrcofield@gmail.com>
2022-09-13 12:17:57 -0400

    First commit

---
+++ MethodsPart2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodsPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a calculator");
+
+            Console.WriteLine(Add(2, 5, 13, 7));
+
+            Console.WriteLine(Multiply(3, 8, 24));
+        }
+
+        //addition method
+        //takes a variable number of int arguments
+        //returns sum of arguments
+        public static int Add(params int[] List)
+        {
+            int sum = 0;
+            for (int i = 0; i < List.Length; i++)
+            {
+                sum += List[i];
+            }
+            return sum;
+        }
+
+        //multiplication method
+        //takes a variable number of int arguments
+        //returns product of arguments
+        public static int Multiply(params int[] List)
+        {
+            int product = 1;
+
+            for (int i = 0; i < List.Length; i++)
+            {
+                product *= List[i];
+            }
+            return product;
+        }
+    }
+}
